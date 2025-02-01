a84f40503d633963945c4d338fb4f2385e49b208
BugHunter-X <askzenai@gmail.com>
2025-02-01 20:06:05 +0100

    Add bugSolution.jl file

---
+++ bugSolution.jl
@@ -0,0 +1,15 @@
+```julia
+function myfunction(x)
+  if x > 0
+    return x^2
+  elseif x == 0
+    return 0
+  else
+    return -x^2 
+  end
+end
+
+println(myfunction(5))
+println(myfunction(-5))
+println(myfunction(0))
+```
